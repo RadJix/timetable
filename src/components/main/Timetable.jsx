604aa5c55db2d2bef11137cbe0bfc8c7fac2191a
RadJix <67233036+RadJix@users.noreply.github.com>
2021-03-24 18:54:58 +0700

    clear Timetable.module.css

--- src/components/main/Timetable.jsx
+++ src/components/main/Timetable.jsx
@@ -4,14 +4,15 @@ import Table from "./table/Table";
 
 
 const Timetable = (props) => {
-    debugger;
+ /*   debugger;
     let tableElements =
         [...props.school.Lecture]
             .map(p => <Table title={p.interfaceLecture.title} teachers={p.interfaceLecture.teachers}
-                           schools={p.interfaceLecture.schools} date={p.interfaceLecture.date} place={p.interfaceLecture.place}/>);
+                           schools={p.interfaceLecture.schools} date={p.interfaceLecture.date} place={p.interfaceLecture.place}/>);*/
     return (
         <div className={c.content}>
-            {tableElements}
+            {/*{tableElements}*/}
+            <Table />
         </div>
     );
 }
